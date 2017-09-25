044f9262dd832b142848e9b1acfab5a0c51fa598
root <nohjinkyu@gmail.com>
2017-09-25 08:10:58 +0000

    god

---
+++ data/WIDERFACE/create_list.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+root_dir=/opt/StorageArray2/dataset/WIDERFACE/
+sub_dir=ImageSets/
+bash_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+for dataset in train val
+do
+  dst_file=$bash_dir/$dataset.txt
+  if [ -f $dst_file ]
+  then
+    rm -f $dst_file
+  fi
+  for name in FACE2017
+  do
+    if [[ $dataset == "test" ]]
+    then
+      continue
+    fi
+    echo "Create list for $name $dataset..."
+    dataset_file=$root_dir/$name/$sub_dir/$dataset.txt
+
+    img_file=$bash_dir/$dataset"_img.txt"
+    cp $dataset_file $img_file
+    sed -i "s/^/$name\/WIDER_$dataset\/images\//g" $img_file
+    sed -i "s/$/.jpg/g" $img_file
+
+    label_file=$bash_dir/$dataset"_label.txt"
+    cp $dataset_file $label_file
+    sed -i "s/^/$name\/Annotations\//g" $label_file
+    sed -i "s/$/.txt/g" $label_file
+
+    paste -d' ' $img_file $label_file >> $dst_file
+
+    rm -f $label_file
+    rm -f $img_file
+  done
+
+  # Generate image name and size infomation.
+  if [ $dataset == "val" ]
+  then
+    $bash_dir/../../build/tools/get_image_size $root_dir $dst_file $bash_dir/$dataset"_name_size.txt"
+  fi
+
+  # Shuffle trainval file.
+  if [ $dataset == "train" ]
+  then
+    rand_file=$dst_file.random
+    cat $dst_file | perl -MList::Util=shuffle -e 'print shuffle(<STDIN>);' > $rand_file
+    mv $rand_file $dst_file
+  fi
+done
