4697d86b3f93eaa55dffb15f564693734dd6c2a2
root <nohjinkyu@gmail.com>
2017-10-09 03:21:20 +0000

    misc

--- models/VGGNet/WiderFace/S3FD_640x640_ohem/run.sh
+++ models/VGGNet/WiderFace/S3FD_640x640_ohem/run.sh
@@ -1,8 +1,9 @@
 cd /opt/StorageArray2/workplace/xwwu-det/workplace/DeepFace/SSDFace/
 ./build/tools/caffe train \
 --solver="models/VGGNet/WiderFace/S3FD_640x640_ohem/solver.prototxt" \
---weights="models/VGGNet/VGG_ILSVRC_16_layers_fc_reduced.caffemodel" \
---gpu 2,3 2>&1 | tee jobs/VGGNet/WiderFace/S3FD_640x640_ohem/VGG_WiderFace_S3FD_640x640_ohem.log
+--snapshot="models/VGGNet/WiderFace/S3FD_640x640_ohem/VGG_WIDER_FACE_S3FD-face_iter_30000.solverstate" \
+--gpu 4,6 2>&1 | tee jobs/VGGNet/WiderFace/S3FD_640x640_ohem/VGG_WiderFace_S3FD_640x640_ohem.log
 
 
 #VGG_WIDER_FACE_S3FD-face_iter_30000.solverstate
+#--weights="models/VGGNet/VGG_ILSVRC_16_layers_fc_reduced.caffemodel" \
